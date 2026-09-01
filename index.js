f13ddbd2f9a5066ec6b932d13930188117dbf8cc
6eansacue <oscaritojovel@gmail.com>
2026-09-01 18:11:48 -0400

    Modal stuff

---
+++ index.js
@@ -0,0 +1,9 @@
+const openModal = () => {
+  const modal = document.getElementById("modal");
+  modal.showModal();
+};
+
+const closeModal = () => {
+  const modal = document.getElementById("modal");
+  modal.close();
+};

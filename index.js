9a2af80c912d197539b1a40995aa4d3e04b811bf
6eansauce <oscaritojovel@gmail.com>
2026-09-09 20:13:21 -0400

    Added movie api

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const searchbar = document.getElementById("searchbar");
+
+searchbar.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const searchbarInput = document.getElementById("searchbarInput");
+  localStorage.setItem("value", searchbarInput.value);
+
+  window.location.href = `${window.location.origin}/search.html`;
+});
+
 const openModal = () => {
   const modal = document.getElementById("modal");
   modal.showModal();
--- search.js
+++ search.js
@@ -1,3 +1,48 @@
+// http://www.omdbapi.com/?apikey=aeff5dc2&
+
+const searchbar = document.getElementById("searchbar");
+
+searchbar.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const searchbarInput = document.getElementById("searchbarInput");
+  localStorage.setItem("value", searchbarInput.value);
+
+  renderMovies();
+});
+
+const main = () => {
+  const value = localStorage.getItem("value");
+  if (value) renderMovies();
+};
+
+const renderMovies = async () => {
+  const value = localStorage.getItem("value");
+  const moviesAPI = await fetch(
+    `http://www.omdbapi.com/?apikey=aeff5dc2&s=${value}`,
+  );
+  const moviesArr = await moviesAPI.json();
+
+  const moviesList = document.querySelector(".movies-list");
+  moviesList.innerHTML = moviesArr.Search.map((movie) =>
+    getMovieHTML(movie),
+  ).join("");
+};
+
+const getMovieHTML = (movie) => {
+  return `<li class="movie">
+    <div class="movie__poster">
+      <img class="img" src="${movie.Poster}" />
+    </div>
+
+    <div class="movie__info">
+      <h3 class="movie__title">${movie.Title}</h3>
+      <h4 class="movie__year">${movie.Year}</h4>
+    </div>
+  </li>
+  `;
+};
+
 const openModal = () => {
   const modal = document.getElementById("modal");
   modal.showModal();
@@ -7,3 +52,5 @@ const closeModal = () => {
   const modal = document.getElementById("modal");
   modal.close();
 };
+
+main();
